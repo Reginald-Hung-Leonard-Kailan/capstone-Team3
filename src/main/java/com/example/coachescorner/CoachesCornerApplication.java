fdf3c724b78eb28c2d1deaf52eb5495a590d1959
Hung Ho <hung.ho902@gmail.com>
2023-04-10 15:56:05 -0500

    feat

---
+++ src/main/java/com/example/coachescorner/CoachesCornerApplication.java
@@ -0,0 +1,13 @@
+package com.example.coachescorner;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoachesCornerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CoachesCornerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/coachescorner/CoachesCornerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.coachescorner;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoachesCornerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

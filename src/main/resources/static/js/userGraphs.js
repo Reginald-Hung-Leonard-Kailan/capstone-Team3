1f9d8ce64b7688d61e89de8a9e6ed1bc35d4f282
Leonard Karoly <leonard.a.karoly@gmail.com>
2023-04-17 11:51:46 -0500

    Created a method to store the user we are looking at

--- src/main/resources/static/js/userGraphs.js
+++ src/main/resources/static/js/userGraphs.js
@@ -1,10 +1,13 @@
 // document.querySelector('meta[name="view"]').content = 2; //Yes this works
-let graphId = document.querySelector('meta[name="view"]').content;
-allInfo(graphId);
-
-async function allInfo(id){
-    let url = "/api/user/" + id;
+allInfo();
+async function allInfo(){
+    let graphId = document.querySelector('meta[name="view"]').content;
+    let url = "/api/user/" + graphId;
     let personalStats = await fetch(url).then(response => response.json())
-                                .then(data => data.clientInformationList);
-    console.log(personalStats);
+                                .then(data => {
+                                    console.log(data);
+                                    return data.clientInformationList;
+                                }).catch(e => console.error(e));
+    console.log("stats are" + personalStats);
 }
+

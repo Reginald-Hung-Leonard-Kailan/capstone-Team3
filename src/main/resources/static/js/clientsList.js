50e8388bcfd9773529a2e6dcde42ce8d2917a8da
Reginald Graham <reg.graham98@gmail.com>
2023-04-24 18:43:34 -0500

    newClientlook

--- src/main/resources/static/js/clientsList.js
+++ src/main/resources/static/js/clientsList.js
@@ -18,34 +18,64 @@ async function start(){
 // MY Clients Card
 function editCard(clients){
     const csrfToken = document.querySelector('meta[name="_csrf"]').content;
-    let id = document.querySelector("#client-holder"), html=``;
+    let id = document.querySelector("#client-holder"), html=``, html1='';
 
     for(let i=0; i<clients.length; i++){
         let client=clients[i];
         html += `
      <div>
-      <div class="client-card">
-        <form  id="delete-client-form" action="/client-false/${client[3]}" method="POST">
-        <input type="hidden" name="_csrf" value="${csrfToken}">
-        <button id="delete-client">❌</button></form>
-        <div><img src="../img/profilePicPlaceholder.png" id="client-image"></div>
-        <div>${client[0]}</div>
-        <hr>
-        <div>${client[1]}</div>
-        <hr>
-        <div>${client[2]}</div>
-        <hr>
-        <div>Coach Created Account*</div>
-        <hr>
-        <div id="client-card-button-holder">
-        <form action="/client-edit/${client[3]}"><button class="search-button client-allign-button">Edit</button></form>
-        <button class="viewer tablinks search-button client-allign-button" onclick="openCity(event, 'Personal-Stats')" value="${client[3]}">Stats</button>
+      <div class="col-md-4">
+        <div class="card profile-card">
+            <form  id="delete-client-form" action="/client-false/${client[3]}" method="POST">
+                <input type="hidden" name="_csrf" value="${csrfToken}">
+                <button id="delete-client">❌</button>
+            </form>
+            <div><img src="../img/profilePicPlaceholder.png" class="avatar-lg" id="client-image"></div>
+            <div class="card-body">
+                <h2>${client[0]} ${client[1]}</h2>
+                <div>${client[2]}</div>
+            </div>
+            <div>Coach Created Account*</div>
+            <div id="client-card-button-holder" class="d-flex align-content-between">
+                <form action="/client-edit/${client[3]}"><button class="search-button client-allign-button">Edit</button></form>
+                <button class="viewer tablinks search-button client-allign-button" onclick="openCity(event, 'Personal-Stats')" value="${client[3]}">Stats</button>
+            </div>
         </div>
       </div>
     </div>`
+
+        html1 += `
+       <div class="col-lg-4 col-sm-6">
+        <div class="card profile-card-3">
+        <form  id="delete-client-form" action="/client-false/${client[3]}" method="POST" style="background-color: #efefef" >
+                <input type="hidden" name="_csrf" value="${csrfToken}">
+                <button id="delete-client" class="d-flex btn-outline-danger" style="border: none;">
+                <svg xmlns="http://www.w3.org/2000/svg" width="64" height="64" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-trash-2 align-middle me-2"><polyline points="3 6 5 6 21 6"></polyline><path d="M19 6v14a2 2 0 0 1-2 2H7a2 2 0 0 1-2-2V6m3 0V4a2 2 0 0 1 2-2h4a2 2 0 0 1 2 2v2"></path><line x1="10" y1="11" x2="10" y2="17"></line><line x1="14" y1="11" x2="14" y2="17"></line></svg>
+</button>
+            </form>
+        <div class="background-block">
+            <img src="../img/client-background.jpg" alt="profile-sample1" class="background img-thumbnail" style="background-size: contain;"/>
+        </div>
+       
+        <div class="profile-thumb-block">
+            <img src="../img/profilePicPlaceholder.png" alt="profile-image" class="profile"/>
+        </div>
+        <div class="card-content">
+            <h2>${client[0]} ${client[1]}<small>${client[2]}</small></h2>
+                <div class="icon-block d-flex justify-content-around">
+                <a href="/client-edit/${client[3]}">
+                    <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-edit align-middle"><path d="M11 4H4a2 2 0 0 0-2 2v14a2 2 0 0 0 2 2h14a2 2 0 0 0 2-2v-7"></path><path d="M18.5 2.5a2.121 2.121 0 0 1 3 3L12 15l-4 1 1-4 9.5-9.5z"></path></svg>
+                </a>
+                <a class="viewer tablinks search-button client-allign-button" onclick="openCity(event, 'Personal-Stats')" href="#" value="${client[3]}">
+                    <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-sliders align-middle"><line x1="4" y1="21" x2="4" y2="14"></line><line x1="4" y1="10" x2="4" y2="3"></line><line x1="12" y1="21" x2="12" y2="12"></line><line x1="12" y1="8" x2="12" y2="3"></line><line x1="20" y1="21" x2="20" y2="16"></line><line x1="20" y1="12" x2="20" y2="3"></line><line x1="1" y1="14" x2="7" y2="14"></line><line x1="9" y1="8" x2="15" y2="8"></line><line x1="17" y1="16" x2="23" y2="16"></line></svg>
+                </a>
+                </div>
+        </div>
+    </div>
+</div>`
     }
 
-    id.innerHTML = html;
+    id.innerHTML = html1;
 
     const viewerBtns = document.getElementsByClassName('viewer');
 

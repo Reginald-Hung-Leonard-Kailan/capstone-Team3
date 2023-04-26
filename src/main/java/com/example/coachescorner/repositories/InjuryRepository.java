30406813cb179abd547ebde0a872cf1bc69a1f04
Hung Ho <hung.ho902@gmail.com>
2023-04-25 23:11:01 -0500

    feat: injury report, update injury, add injury, update add client and update client view to the new look

--- src/main/java/com/example/coachescorner/repositories/InjuryRepository.java
+++ src/main/java/com/example/coachescorner/repositories/InjuryRepository.java
@@ -1,8 +1,11 @@
 package com.example.coachescorner.repositories;
 
 import com.example.coachescorner.model.Injury;
+import com.example.coachescorner.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface InjuryRepository extends JpaRepository<Injury, Long> {
     Injury findById(long id);
+
+    Injury findByUser(User user);
 }
--- src/main/resources/static/js/stats-render.js
+++ src/main/resources/static/js/stats-render.js
@@ -24,11 +24,11 @@ async function allInfo(id = viewId){
         .then(data => {
             // const {username, email, firstName, lastName, phoneNumber, bio, profilePicture} = data;
             viewerInfo(data);
+            showInjury(data);
             console.log("data is: " + data);
             data = data.clientInformationList
             return data.sort((b, a) => new Date(a.date) - new Date(b.date));
         }).catch(e => console.error(e));
-
     personalStats.map(data => {
         switch (data.type){
             case "workoutPlan":
@@ -64,6 +64,7 @@ function viewerInfo(data){
     let {username, email, firstName, lastName, phoneNumber, bio, profilePicture} = data,
         id = document.querySelector("#personal-info"),
         html = "";
+    console.log(data.injuries);
 
     html = `<div class="card-content"> 
 <h1>First/Last: ${firstName} ${lastName}</h1>
@@ -73,6 +74,34 @@ function viewerInfo(data){
 <div>username:  ${username}</div>
 <div>email: ${email}</div>
 </div>`;
+
+    id.innerHTML = html;
+}
+
+//Injury Info card
+
+function showInjury(data){
+    let {injuryDate, status, title, description} = data.injuries,
+        id = document.querySelector("#injury-info"),
+        html = "";
+
+    for(let i = 0 ; i < data.injuries.length ; i++ ){
+
+        let injury = data.injuries[i];
+        html += `<tr>
+                  <td>Injury</td>
+                  <td class="d-none d-xl-table-cell">${injury.injuryDate}</td>
+                  <td class="d-none d-xl-table-cell">${injury.title}</td>
+                  <td><span id="injury-status" style="color: #0a0a0a">${injury.status}</span></td>
+                  <td class="d-none d-md-table-cell">${injury.description}</td>
+                  <td class="d-none d-md-table-cell"><a href="/injury/edit/${injury.id}"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-pencil-square" viewBox="0 0 16 16">
+  <path d="M15.502 1.94a.5.5 0 0 1 0 .706L14.459 3.69l-2-2L13.502.646a.5.5 0 0 1 .707 0l1.293 1.293zm-1.75 2.456-2-2L4.939 9.21a.5.5 0 0 0-.121.196l-.805 2.414a.25.25 0 0 0 .316.316l2.414-.805a.5.5 0 0 0 .196-.12l6.813-6.814z"/>
+  <path fill-rule="evenodd" d="M1 13.5A1.5 1.5 0 0 0 2.5 15h11a1.5 1.5 0 0 0 1.5-1.5v-6a.5.5 0 0 0-1 0v6a.5.5 0 0 1-.5.5h-11a.5.5 0 0 1-.5-.5v-11a.5.5 0 0 1 .5-.5H9a.5.5 0 0 0 0-1H2.5A1.5 1.5 0 0 0 1 2.5v11z"/>
+</svg></a></td> 
+                </tr>`;
+    }
+
+
 
     id.innerHTML = html;
 }
